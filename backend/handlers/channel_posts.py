c5ba2694c833d122fbd9991d248dfc06cb621257
Danya Putilin <danyaputilin@yandex.ru>
2026-04-23 19:14:03 +0300

    channels update

--- backend/handlers/channel_posts.py
+++ backend/handlers/channel_posts.py
@@ -6,6 +6,6 @@ import os
 @bot.channel_post_handler(func=lambda x: True)
 def channel_posts_handler(post):
     """Посты в каналах"""
-    answer = f"New post in channel '{post.chat.title}' - {post.chat.username} at {datetime.datetime.today()}"
+    answer = f"New post in channel '{post.chat.title}' (@{post.chat.username}) at {datetime.datetime.today()}"
     bot.send_message(os.getenv("ADMIN_ID"), answer)
     print(answer)
